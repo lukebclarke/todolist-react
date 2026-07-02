68ebfae7f727b6dcf5f970e9ca43263b492b625b
lukebclarke <luke616logins@gmail.com>
2026-07-02 14:15:45 +0100

    Implemented placeholder callback when clicking todo item

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { TodoList } from "./data/todos"
 
 function App() {
 
+  function setToDoCompleted(id: number, completed: boolean) {
+    alert(`todo with ${id} is now ${completed ? "completed" : "not completed"}`)
+  }
+
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">
         Your ToDos
       </h1>
-      <div className="max-w-lg mx-auto">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {
             TodoList.map(todo => (
-              <ToDoItem todo={todo}/>
+              <ToDoItem 
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setToDoCompleted}/>
             ))
           }
         </div>
--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,15 +1,18 @@
 import type { ToDo } from "../types/todo"
 
 interface ToDoItemProps {
-    todo: ToDo;
+    todo: ToDo,
+    onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function ToDoItem({todo}: ToDoItemProps) {
+export default function ToDoItem({todo, onCompletedChange}: ToDoItemProps) {
     return (
         <div>
             <label className="flex items-center gap-2 border rounded-md p-2 border-gray-500 bg-white hover:bg-slate-50">
                 <input
                 type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
                 className="scale-125"
                 />
             <span className={todo.completed ? "line-through text-gray-500" : ""}>
