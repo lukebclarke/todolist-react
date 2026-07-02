f5afcb8588dc110660f42fa946d78ba107452c38
lukebclarke <luke616logins@gmail.com>
2026-07-02 14:41:54 +0100

    Items are now clickable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import ToDoItem from "./components/ToDoItem"
 import { TodoList } from "./data/todos"
 
 function App() {
 
+  const [todos, setTodos] = useState(TodoList)
+
   function setToDoCompleted(id: number, completed: boolean) {
-    alert(`todo with ${id} is now ${completed ? "completed" : "not completed"}`)
+    setTodos((prevTodos) => 
+      prevTodos.map((todo) => (
+        todo.id === id ? {...todo, completed} : todo)
+      )
+  )
   }
 
   return (
@@ -15,10 +22,10 @@ function App() {
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {
-            TodoList.map(todo => (
+            todos.map(todo => (
               <ToDoItem 
               key={todo.id}
-              todo={todo}
+              todo={todo} 
               onCompletedChange={setToDoCompleted}/>
             ))
           }
