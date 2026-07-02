20355c6094e9bc51076b81f1a24dad1b11a44b72
lukebclarke <luke616logins@gmail.com>
2026-07-02 16:16:35 +0100

    Added overflow to end of list

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
   }
 
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">
         Your ToDos
       </h1>
