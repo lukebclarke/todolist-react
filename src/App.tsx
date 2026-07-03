a973d787a19341c7519945c328d3517db473a963
lukebclarke <luke616logins@gmail.com>
2026-07-03 15:18:50 +0100

    Improved navbar visuals

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <Navbar/>
-      <h1 className="font-bold pt-20 text-3xl text-center">
+      <h1 className="font-bold pt-10 text-3xl text-center">
         Your ToDos
       </h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className='bg-slate-800 shadow-lg flex items-center justify-around py-3 px-16 fixed top-0 left-0 w-full'>
+        <nav className='bg-slate-800 shadow-lg flex items-center justify-between py-2 px-32 fixed top-0 left-0 w-full'>
             <Link to="/">
                 <span className="font-semibold text-lg flex items-center gap-3 text-blue-400">
                     <HiClipboardList className="text-3xl"/>
@@ -16,11 +16,11 @@ export default function Navbar() {
                     Tasks
                 </Link>
 
-                <Link to="/" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 hover:bg-slate-700 transition duration-300">
+                <Link to="/history" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 hover:bg-slate-700 transition duration-300">
                     History
                 </Link>
 
-                <Link to="/" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 hover:bg-slate-700 transition duration-300">
+                <Link to="/calendar" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 hover:bg-slate-700 transition duration-300">
                     Calendar
                 </Link>
             </div>
