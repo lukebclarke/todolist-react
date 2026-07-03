74bc0b2e6a0fe1303b3459456bcb8c264a537cac
lukebclarke <luke616logins@gmail.com>
2026-07-03 11:47:06 +0100

    Created ToDo hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-import { useEffect, useState } from "react"
-import { TodoList } from "./data/todos"
 import AddToDoForm from "./components/AddToDoForm"
 import ToDoList from "./components/ToDoList"
 import ToDoSummary from "./components/ToDoSummary"
-import type { ToDo } from "./types/todo"
+import useToDos from "./hooks/useToDo"
 
 function App() {
-
-  const [todos, setTodos] = useState(() => {
-    const savedTodos: ToDo[] = JSON.parse(localStorage.getItem("todos") || "[]");
-    return savedTodos.length > 0 ? savedTodos : TodoList
-  });
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
-
-  function setToDoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) => 
-      prevTodos.map((todo) => (
-        todo.id === id ? {...todo, completed} : todo)
-      )
-  )
-  }
-
-  function addToDo(title: string) {
-    setTodos((prevTodos) => [
-    {
-      id: Date.now(),
-      title,
-      completed: false
-    },
-    ...prevTodos
-    ])
-
-  }
-
-  function removeToDo(id: number) {
-    setTodos((prevTodos) => 
-      prevTodos.filter(todo => todo.id !== id)
-  )
-  }
-
-  function deleteAllCompleted() {
-    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed))
-  }
+  const {
+    todos,
+    setToDoCompleted,
+    addToDo,
+    removeToDo,
+    deleteAllCompleted
+  } = useToDos();
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
---
+++ src/hooks/useToDo.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import type { ToDo } from "../types/todo";
+import { TodoList } from "../data/todos";
+
+export default function useToDos() {
+
+    const [todos, setTodos] = useState(() => {
+    const savedTodos: ToDo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return savedTodos.length > 0 ? savedTodos : TodoList
+    });
+
+    useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
+
+    function setToDoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => 
+        prevTodos.map((todo) => (
+        todo.id === id ? {...todo, completed} : todo)
+        )
+    )
+    }
+
+    function addToDo(title: string) {
+    setTodos((prevTodos) => [
+    {
+        id: Date.now(),
+        title,
+        completed: false
+    },
+    ...prevTodos
+    ])
+
+    }
+
+    function removeToDo(id: number) {
+    setTodos((prevTodos) => 
+        prevTodos.filter(todo => todo.id !== id)
+    )
+    }
+
+    function deleteAllCompleted() {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed))
+    }
+
+    return {
+        todos,
+        setToDoCompleted,
+        addToDo,
+        removeToDo,
+        deleteAllCompleted
+    }
+
+}
